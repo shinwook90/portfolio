b12c5641b804abf1170554aec4aa5458913eb4fe
shinwook90 <65443959+shinwook90@users.noreply.github.com>
2020-06-26 01:01:53 +0900

    Js : scroll

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-'use strict'
-
-const navbar = document.querySelector('#navbar');
+"use strict";
+//Make navbar transparent when it is on the top
+const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log('navbarHeight: ${navbarHeight}');
+document.addEventListener("scroll", () => {
     if (window.scrollY > navbarHeight) {
-        navbar.classList.add('navbar--dark');
+        navbar.classList.add("navbar--dark");
     } else {
-        navbar.classList.remove('navbar--dark')
+        navbar.classList.remove("navbar--dark");
     }
+});
+
+const navbarMenu = document.querySelector('.navbar_menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link)
+    scrollTo.scrollIntoView({
+        behavior: "smooth"
+    });
 });
